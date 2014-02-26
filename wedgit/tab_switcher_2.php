07dca7092f3e8317ee72c396f422e7f2eef8a39d
ihuguowei <ihuguowei@gmail.com>
2014-02-26 15:25:31 +0800

    友情链接

--- wedgit/tab_switcher_2.php
+++ wedgit/tab_switcher_2.php
@@ -3,44 +3,42 @@
  <div class="tab">
     <div class="tab-switcher">
         <ul class="tab-swither-container">
-            <li class="current tab-switcher-list">最近评论</li>
-            <li class="tab-switcher-list">友链</li>
+            <li class=" tab-switcher-list">最近评论</li>
+            <li class="tab-switcher-list current">友链</li>
             <li class="tab-switcher-list">评论墙</li>
         </ul>
     </div>
     <div class="tab-container">
         <ul class="tab-content">
-            <li class="tab-content-list current">
+            <li class="tab-content-list ">
+                <h3 class="widget-title">最近的评论！</h3>
                 <ul class="new-comments">
-                    <h3 class="widget-title">最近的评论！</h3>
                     <?php simpleway_newcomments(6); ?>
                 </ul>
             </li>
-            <li class="tab-content-list">
-                <ul class="blog roll">
-                    <h3 class="widget-title">有情才链接！</h3>
-            	   <?php wp_list_bookmarks(array(
-                        'orderby'          => 'count', // 排序方式'name' - Default、'id'、'slug'、'count'
-                        'order'            => 'ASC',
-                        'limit'            => -1,  // 显示的数目
-                        'category'         => ' ', // 是否以分类形式显示
-                        'exclude_category' => ' ', // 排除的分类
-                        'category_name'    => ' ',  
-                        'hide_invisible'   => 1,
-                        'show_updated'     => 0,
-                        'show_images'      => 1,
-                        'echo'             => true,
-                        'categorize'       => 1,
-                        'title_li'         => __('Bookmarks'),
-                        'title_before'     => '<h2>',
-                        'title_after'      => '</h2>',
-                        'category_orderby' => 'name',
-                        'category_order'   => 'ASC',
-                        'class'            => 'linkcat',
-                        'category_before'  => '<li id=%id class=%class>',
-                        'category_after'   => '</li>'
-                   )); ?>
-                </ul>
+            <li class="tab-content-list current">
+                <?php wp_list_bookmarks(array(
+                    'orderby'          => 'name',
+                    'order'            => 'ASC',
+                    'limit'            => -1,
+                    'category'         => ' ',
+                    'exclude_category' => ' ',
+                    // 'category_name'    => ' ',
+                    'hide_invisible'   => 1,
+                    'show_updated'     => 0,
+                    'show_description' => 0,
+                    'show_name'        => 1,
+                    'echo'             => 1,
+                    'categorize'       => 1,
+                    // 'title_li'         => __('Bookmarks'),
+                    'title_before'     => '<h3 class="widget-title">',
+                    'title_after'      => '</h3>',
+                    'category_orderby' => 'name',
+                    'category_order'   => 'ASC',
+                    'class'            => 'linkcat',
+                    'category_before'  => '',
+                    'category_after'   => ''
+                )); ?> 
             </li>
             <li class="tab-content-list">
                 <h3 class="widget-title">本月灌水王！</h3>
